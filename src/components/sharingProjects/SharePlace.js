ad2424a11596d7fbce19d3c35271aef374dad3d7
i343SPARK <pablosalas_@outlook.com>
2022-03-21 22:30:03 -0600

    Adelanto Vergas

--- src/components/sharingProjects/SharePlace.js
+++ src/components/sharingProjects/SharePlace.js
@@ -4,16 +4,28 @@ export const SharePlace = () => {
   return (
     <section className='generalContent'>
         <div className='shareDiv'>
-            <h2>Groups</h2>
+            <h2>Grupos</h2>
             <div className='numGroup'>
                 <span className='imgGroup'></span>
-                <p>Grupo 1</p>
+                <h3>Grupo 1</h3>
+            </div>
+            <div className='numGroup'>
+                <span className='imgGroup'></span>
+                <h3>Grupo 2</h3>
+            </div>
+            <div className='numGroup'>
+                <span className='imgGroup'></span>
+                <h3>Grupo 3</h3>
+            </div>
+            <div className='numGroup'>
+                <span className='imgGroup'></span>
+                <h3>Grupo 4</h3>
             </div>
         </div>
         <div className='shareDiv'>
-            <h2>Apps</h2>
+            <h2>Proyectos</h2>
             <div>
-
+                
             </div>
         </div>
     </section>
