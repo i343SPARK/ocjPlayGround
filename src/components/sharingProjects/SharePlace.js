07163bcc97005f65d2d9b3501ce6587071b02d8d
i343SPARK <pablosalas_@outlook.com>
2022-03-22 13:40:33 -0600

    Adelanto Vergas

--- src/components/sharingProjects/SharePlace.js
+++ src/components/sharingProjects/SharePlace.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { ModalHook } from '../../hooks/ModalHook'
 
 export const SharePlace = () => {
+    const [isOpenModal1, openModal1, closeModal1] = ModalHook(false);
+
   return (
     <section className='generalContent'>
         <div className='shareDiv'>
@@ -36,13 +39,43 @@ export const SharePlace = () => {
             <h2>Proyectos</h2>
             <article className='overflowGrp1'>
                 <div>
-                    <button className='buttonProyect'>
+                    <button onClick={openModal1} className='buttonProyect'>
                         <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
                         <h3>Proyecto 1</h3>
                     </button>
-                    <button className='buttonProyect'>
+                    <button onClick={openModal1} className='buttonProyect'>
                         <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
-                        <h3>Proyecto 1</h3>
+                        <h3>Proyecto 2</h3>
+                    </button>
+                </div>
+                <div>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 3</h3>
+                    </button>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 4</h3>
+                    </button>
+                </div>
+                <div>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 5</h3>
+                    </button>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 6</h3>
+                    </button>
+                </div>
+                <div>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 7</h3>
+                    </button>
+                    <button onClick={openModal1} className='buttonProyect'>
+                        <img width='10px' className='img' src='http://cdn.eso.org/images/screen/sunrise-shadow-cc.jpg'/>
+                        <h3>Proyecto 8</h3>
                     </button>
                 </div>
             </article>
