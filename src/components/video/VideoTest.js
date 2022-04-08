4edb723f2c4f24c1606c5ba941f87c072d037417
i343SPARK <pablosalas_@outlook.com>
2022-04-08 12:11:26 -0500

    Tlayulas Numericas

--- src/components/video/VideoTest.js
+++ src/components/video/VideoTest.js
@@ -4,9 +4,34 @@ import ReactPlayer from 'react-player'
 
 export const VideoTest = () => {
 
+  let [speed, setSpeed] = useState(1)
+  let [names, setNames] = useState("play")
   const [condition, setCondition] = useState(false);
   const [condition2, setCondition2] = useState(false);
+  const [condition3, setCondition3] = useState(false);
   
+  const speedOne = () => {
+    setSpeed(speed = 1)
+  }
+  const speedOneFive = () => {
+    setSpeed(speed = 1.5)
+  }
+  const speedTwo = () => {
+    setSpeed(speed = 2)
+  }
+
+  const onPlayName = () => {
+    if(condition){
+      setNames("Play")
+    }
+
+    if(!condition){
+     setNames("Pause")
+    }
+
+    setCondition(!condition)
+  }
+
   return (
     <section className='videoSection'>
       <div className='videoSources'>
@@ -17,12 +42,19 @@ export const VideoTest = () => {
         url="https://www.dropbox.com/s/1xcg0a9wxorpi2x/ego-prueba-el-platillo-que-remy-le-prepara-ratatouille.mp4?dl=0"
         playing={condition}
         pip={condition2}
+        muted ={condition3}
+        playbackRate={speed}
         
         />
+      <div className='videoBar'>
+        <button className='videoButton' onClick={onPlayName}>{names}</button>
+        <button className='videoButton' onClick={() => setCondition2(!condition2)}>PiP</button>
+        <button className='videoButton' onClick={() => setCondition3(!condition3)}>Muted</button>
+        <button className='videoButton' onClick={speedOne}>x1</button>
+        <button className='videoButton' onClick={speedOneFive}>x1.5</button>
+        <button className='videoButton' onClick={speedTwo}>x2</button>
       </div>
-      <div>
-        <button style={{width: "300px", height: "300pxs"}} onClick={() => setCondition(!condition)}>Play</button>
-        <button style={{width: "300px", height: "300pxs"}} onClick={() => setCondition2(!condition2)}>PiP</button>
+      <input type="range"></input>
       </div>
     </section>
   )
